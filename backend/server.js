1a3dfa493f98cd4af93426e95144e21226a3bdcc
JacostaAv <juancaavac7@gmail.com>
2026-02-11 20:10:54 -0500

    Actualización del proyecto antes del despliegue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Servir archivos estáticos
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
 
 // Ruta POST para guardar mensajes
@@ -71,9 +71,9 @@ app.get("/messages", (req, res) => {
     res.send(html);
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+//app.get("*", (req, res) => {
+//    res.sendFile(path.join(__dirname, "public", "index.html"));
+//});
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
